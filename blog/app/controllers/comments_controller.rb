521a7e8e84700d956004b58042a29c2b404ef332
Patrick McGannon <pmcgannon22@gmail.com>
2013-04-28 22:26:23 -0500

    Fixes for commenting.

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -2,6 +2,6 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment])
-    redirect_to_post_path(@post)
+    redirect_to post_path(@post)
   end
 end
